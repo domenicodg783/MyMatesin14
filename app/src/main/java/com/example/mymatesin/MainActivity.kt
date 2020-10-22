a4482baf36b16daa1d08edfd666758af28fe59ec
Alessandro Iannone <iann270599@gmail.com>
2020-10-22 09:16:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mymatesin/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mymatesin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler()
+    }
+}
+
