89180db990500aa866f7e28c5d6d4c45687bad18
Alessandro Iannone <iann270599@gmail.com>
2020-10-22 19:00:50 +0200

    22/10

--- app/src/main/java/com/example/mymatesin/SplashActivity.kt
+++ app/src/main/java/com/example/mymatesin/SplashActivity.kt
@@ -1,5 +1,8 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.mymatesin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -7,11 +10,15 @@ import android.os.Handler
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
         supportActionBar?.hide()
 
-        Handler()
+         Handler().postDelayed({
+            val intent = Intent(this@MainActivity, Login::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
 
